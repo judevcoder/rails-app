a7db7d4420fcba907e37fb576b3c6cb9e3dd8c8c
Giant <giantdev118@gmail.com>
2017-05-09 16:21:35 +0200

    Implement boostrap treeview

--- db/schema.rb
+++ db/schema.rb
@@ -169,7 +169,7 @@ ActiveRecord::Schema.define(version: 20170504095756) do
     t.boolean  "m_date_of_appointment",                 default: false
     t.boolean  "m_date_of_commission",                  default: false
     t.integer  "user_id"
-    t.boolean  "has_comma"
+    t.boolean  "has_comma",                             default: false
     t.index ["deleted_at"], name: "index_entities_on_deleted_at", using: :btree
     t.index ["user_id"], name: "index_entities_on_user_id", using: :btree
   end
