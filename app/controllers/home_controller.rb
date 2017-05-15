7820d83d83fdeba50a5b6b6e0233045fc9a15bbe
Ashwin Mangale <ashwin.mangale@gmail.com>
2017-05-15 11:10:52 +0530

    fix the duplicate sign in message on /

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
+  skip_before_filter :authenticate_user!, only: [:index]
+
   def index
-    redirect_to properties_path
+    if current_user
+      redirect_to properties_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def set_user_indexing
