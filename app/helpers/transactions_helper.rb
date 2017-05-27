1e8664da9a0779a7f24851e38d7c3d7e6975555b
Giant <giantdev118@gmail.com>
2017-05-27 14:34:40 +0200

    First property should be displayed on neogations tab

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -4,17 +4,17 @@ module TransactionsHelper
             is_sale = true
         elsif property_type == 'purchase'
             is_sale = false
-            else
-                is_sale = 'undefined'
+        else
+            is_sale = 'undefined'
         end
-        
+
         if is_sale != 'undefined'
             transaction_property_ids = TransactionProperty.where(transaction_main_id: trans_main_id, is_sale: is_sale).pluck(:property_id)
-            properties = Property.where(id: transaction_property_ids).order(created_at: :desc)
+            properties = Property.where(id: transaction_property_ids).order(created_at: :asc)
         else
-            properties = nil 
+            properties = nil
         end
-        
+
         return properties
     end
 
@@ -27,7 +27,7 @@ module TransactionsHelper
     def get_property(property_id)
         return Property.find(property_id)
     end
-    
+
     def get_recent_counteroffer_price(transaction_property_offer)
         last_offered_price = transaction_property_offer.counteroffers.where("offered_price IS NOT NULL AND offered_date IS NOT NULL").order(updated_at: :asc).last.try(:offered_price)
         if last_offered_price
@@ -38,7 +38,7 @@ module TransactionsHelper
     end
 
     def get_transaction_property_offeror(transaction_property_offer)
-        @purchaser = Contact.where(id: transaction_property_offer.relinquishing_purchaser_contact_id).first 
+        @purchaser = Contact.where(id: transaction_property_offer.relinquishing_purchaser_contact_id).first
         if @purchaser.present?
             return Contact.where(id: transaction_property_offer.relinquishing_purchaser_contact_id).first
         else
@@ -74,7 +74,7 @@ module TransactionsHelper
             return ''
         end
     end
-    
+
     def is_property_closed?(transaction_id, property_id)
       retVal = false
       p = TransactionProperty.where(transaction_id: transaction_id, property_id: property_id).first
