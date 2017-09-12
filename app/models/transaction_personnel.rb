7e779e49d80a2326bb9b991d7d4b53f09e192582
Desk Star <wangway127@gmail.com>
2017-09-12 22:31:06 +1200

    For every tab in Inspection except for Seller Documentation we need three subtabs Personnel, Files, Tasks

--- app/models/transaction_personnel.rb
+++ app/models/transaction_personnel.rb
@@ -5,12 +5,4 @@ class TransactionPersonnel < ApplicationRecord
 
   FIXED_TITLE = ['Title', 'Survey', 'Environmental', 'Zoning']
 
-  # def create_contact(t)
-  #   self.contacts.find_by(object_title: t) || ContactOverride.create(object_title: t, type_id: self.id)
-  # end
-
 end
-
-# class ContactOverride < ApplicationRecord
-#   self.table_name = 'contacts'
-# end
